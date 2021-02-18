b5ccbe29eb65304533165949a1d76b07a0d956cd
shyam <shyam.kachhadiya@sulok.in>
2021-02-18 21:10:49 +0530

    Banking System

---
+++ app/src/main/java/com/sleepingpandaaa/bankingsystem/DatabaseHelper.java
@@ -0,0 +1,86 @@
+package com.sleepingpandaaa.bankingsystem;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private String TABLE_NAME = "user_table";
+    private String TABLE_NAME1 = "transfers_table";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "User.db", null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (PHONENUMBER INTEGER PRIMARY KEY ,NAME TEXT,BALANCE DECIMAL,EMAIL VARCHAR)");
+        db.execSQL("create table " + TABLE_NAME1 +" (TRANSACTIONID INTEGER PRIMARY KEY AUTOINCREMENT,DATE TEXT,FROMNAME TEXT,TONAME TEXT,AMOUNT DECIMAL,STATUS TEXT)");
+        db.execSQL("insert into user_table values(9999999999,'Sahib',9000,'sahib@gmail.com')");
+        db.execSQL("insert into user_table values(9898989898,'Milan',5500,'milan@gmail.com')");
+        db.execSQL("insert into user_table values(9797979797,'Rikta',3500,'rikta@gmail.com')");
+        db.execSQL("insert into user_table values(9696969696,'Parth',8700,'parth@gmail.com')");
+        db.execSQL("insert into user_table values(9595959595,'Gautam',3900,'gautam@gmail.com')");
+        db.execSQL("insert into user_table values(9494949494,'Harsh',800,'harsh@gmail.com')");
+        db.execSQL("insert into user_table values(9393939393,'Ram',1900,'ram@gmail.com')");
+        db.execSQL("insert into user_table values(9292929292,'Anushka',8900,'anushka@gmail.com')");
+        db.execSQL("insert into user_table values(9191919191,'Tushar',7400,'tushar@gmail.com')");
+        db.execSQL("insert into user_table values(9090909090,'Ajay',6520,'ajay@gmail.com')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME1);
+        onCreate(db);
+    }
+
+    public Cursor readalldata(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from user_table", null);
+        return cursor;
+    }
+
+    public Cursor readparticulardata(String phonenumber){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from user_table where phonenumber = " +phonenumber, null);
+        return cursor;
+    }
+
+    public Cursor readselectuserdata(String phonenumber) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from user_table except select * from user_table where phonenumber = " +phonenumber, null);
+        return cursor;
+    }
+
+    public void updateAmount(String phonenumber, String amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("update user_table set balance = " + amount + " where phonenumber = " +phonenumber);
+    }
+
+    public Cursor readtransferdata(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from transfers_table", null);
+        return cursor;
+    }
+
+    public boolean insertTransferData(String date,String from_name, String to_name, String amount, String status){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("DATE", date);
+        contentValues.put("FROMNAME", from_name);
+        contentValues.put("TONAME", to_name);
+        contentValues.put("AMOUNT", amount);
+        contentValues.put("STATUS", status);
+        Long result = db.insert(TABLE_NAME1, null, contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
